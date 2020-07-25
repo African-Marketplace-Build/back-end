06cd75ad9a9675fc9bd2092db5c623eb12ca5172
Luis Pineda <lspnd00@gmail.com>
2020-07-24 23:47:23 -0400

    login/register endpoints

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config() // add this line as the first thing to run1
+// const path = require('./public/index.html'
+const express = require('express')
+var cookieParser = require('cookie-parser')
+const server = express()
+const usersRouter = require('./users/users-router')
+const itemsRouter = require('./items/items-router')
+const authRouter = require('./auth/auth-router')
+const PORT = process.env.PORT || 5000
+const cors = require('cors')
+
+server.use(cookieParser())
+server.use(express.static(__dirname + '/public'))
+server.use(express.json())
+server.use(cors())
+server.use('/api/', authRouter)
+server.use('/api/', authRouter)
+server.use('/api/', usersRouter)
+server.use('/api/', itemsRouter)
+
+server.get('/', (req, res) => {
+  res.sendFile(__dirname, +'./public/index.html')
+})
+server.use((req, res) => {
+  res.status(404).json(`This end point: ${req.url} does not exist in our api`)
+})
+
+// if (!module.parent) {
+server.listen(PORT, () => {
+  console.log(`Listening on at: http://localhost:${PORT}/`)
+})
+// }
---
+++ knexfile.js
@@ -0,0 +1,41 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true, // needed for sqlite
+    connection: {
+      filename: './data/auth.db3',
+    },
+    migrations: {
+      directory: '../back-end/data/migrations',
+    },
+    seeds: {
+      directory: '../back-end/data/seeds',
+    },
+
+    // this is needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done) // turn on FK enforcement
+      },
+    },
+  },
+  // -----------------------------testing-----------------------
+  testing: {
+    client: 'sqlite3',
+    connection: { filename: './data/market-test.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: '../back-end/data/migrations',
+    },
+    seeds: { directory: '../back-end/data/seeds' },
+
+    // this is needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done) // turn on FK enforcement
+      },
+    },
+  },
+}
