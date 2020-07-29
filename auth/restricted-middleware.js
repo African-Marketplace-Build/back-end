095b619c473bcabce4d9d5c83cbd07fdb7191b3c
Luis Pineda <lspnd00@gmail.com>
2020-07-28 23:03:38 -0400

    data 2nd

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,20 +3,16 @@ const jwt = require('jsonwebtoken')
 // checking if user is authorized by verifying token is in request header
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization
+  const token = req.cookies.token
 
   if (token) {
-    jwt.verify(
-      token,
-      process.env.SECRET || 'HU2q2hkB3nMcYkMOyGAXLKSyAABdfWuoRk62qudbwYw',
-      err => {
-        if (err) {
-          res.status(401).json({ message: 'Invalid Credentials' })
-        } else {
-          next()
-        }
+    jwt.verify(token, process.env.SECRET, err => {
+      if (err) {
+        res.status(401).json({ message: 'Invalid Credentials' })
+      } else {
+        next()
       }
-    )
+    })
   } else {
     res.status(400).json({ message: 'No token provided' })
   }
