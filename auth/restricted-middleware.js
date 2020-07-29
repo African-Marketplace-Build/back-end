71d996b367d3c36a05a7928d91a5e4b6669a5fa5
Luis Pineda <lspnd00@gmail.com>
2020-07-28 23:45:42 -0400

    update 2

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken')
 // checking if user is authorized by verifying token is in request header
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.token
+  const token = req.cookies.token || req.headers.authorization
 
   if (token) {
-    jwt.verify(token, process.env.SECRET, err => {
+    jwt.verify(token, process.env.SECRET || 'secret key', err => {
       if (err) {
         res.status(401).json({ message: 'Invalid Credentials' })
       } else {
