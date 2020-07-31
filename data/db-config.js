c7bade5bf45d3a0be378719b6648e3f71389e477
Luis Pineda <lspnd00@gmail.com>
2020-07-30 23:29:25 -0400

    update 11

--- data/db-config.js
+++ data/db-config.js
@@ -2,6 +2,6 @@ const knex = require('knex')
 
 const knexConfig = require('../knexfile')
 
-const dbEnv = process.env.DB_ENV || 'development'
+const dbEnv = process.env.DB_ENV
 
 module.exports = knex(knexConfig[dbEnv])
